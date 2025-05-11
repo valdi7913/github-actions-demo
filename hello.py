7422df44372ef70be1a7f3100b8ceb5e9d383e0a
valdi7913 <valdi7913@gmail.com>
2025-05-11 17:48:07 +0000

    Dangus, gleymdir requirements og venv

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@
 import requests
 
 print("Hello from GitHub Actions!")
-print("Status code from example.com:", requests.get("https://example.com").status_code)
+print("Status code from example.com:", requests.get("https://mbl.is").status_code)
